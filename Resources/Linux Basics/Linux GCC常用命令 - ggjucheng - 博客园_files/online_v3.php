e224bf4df5aa1d0b9168b947f3a4be6ce0aa99da
Friedrich <490626598@qq.com>
2017-09-14 15:46:47 +0800

    Linux Basics

---
+++ Resources/Linux Basics/Linux GCC常用命令 - ggjucheng - 博客园_files/online_v3.php
@@ -0,0 +1,6 @@
+var node=document.getElementById('cnzz_stat_icon_'+4902471);
+if(node){
+    node.innerHTML="<a href=http://www.cnzz.com/stat/website.php?web_id=4902471 target=_blank>站长统计</a>";
+}else{
+    document.write("<a href=http://www.cnzz.com/stat/website.php?web_id=4902471 target=_blank>站长统计</a>");
+}
